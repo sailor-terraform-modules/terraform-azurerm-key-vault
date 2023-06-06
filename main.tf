2f1a0659e11eb512e3e26cfc1fec48c3a5812ccd
RaviShankar <ravishankar@Ravi-MacBook-Air.local>
2023-06-06 12:36:35 +0530

    fix validate

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "azurerm_key_vault" "key_vault" {
 
 resource "azurerm_storage_account" "storage" {
   name                     = var.storage_name
-  resource_group_name      = var.resource_group
+  resource_group_name      = var.resource_group_name
   location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
