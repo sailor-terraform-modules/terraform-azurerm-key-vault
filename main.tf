7c889e0aa1595933ae61e9e7891b3b9e17073902
RaviShankar <ravishankar@Ravi-MacBook-Air.local>
2023-06-06 12:32:37 +0530

    fix logging

--- main.tf
+++ main.tf
@@ -30,7 +30,13 @@ resource "azurerm_key_vault" "key_vault" {
   }
 }
 
-
+resource "azurerm_storage_account" "storage" {
+  name                     = var.storage_name
+  resource_group_name      = var.resource_group
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
 
 resource "azurerm_monitor_diagnostic_setting" "monitor" {
   name               = var.monitor_name
