f7fbeacb8d76eae23d37831118f7d58cb63ff195
RaviShankar <ravishankar@Ravi-MacBook-Air.local>
2023-06-06 12:24:39 +0530

    inital commit

---
+++ main.tf
@@ -0,0 +1,32 @@
+data "azurerm_client_config" "current" {}
+resource "azurerm_key_vault" "key_vault" {
+  name                          = var.name
+  location                      = var.location
+  resource_group_name           = var.resource_group_name
+  enabled_for_disk_encryption   = true
+  tenant_id                     = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days    = var.soft_delete_retention_days
+  purge_protection_enabled      = var.purge_protection_enabled
+  enable_rbac_authorization     = false
+  public_network_access_enabled = var.public_network_access_enabled
+  sku_name                      = var.sku_name
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+    key_permissions = [
+      "SetRotationPolicy",
+    ]
+    secret_permissions = [
+      "Get", "Set", "List", "Delete", "Restore"
+    ]
+    storage_permissions = [
+      "Get",
+    ]
+  }
+  network_acls {
+    bypass         = "AzureServices"
+    default_action = "Allow"
+
+  }
+}
+
---
+++ variable.tf
@@ -0,0 +1,34 @@
+variable "name" {
+  type        = string
+  description = "key vault name"
+}
+variable "location" {
+  type        = string
+  description = "location of the key vault"
+}
+variable "resource_group_name" {
+  type        = string
+  description = "resource group of the key vault"
+}
+variable "soft_delete_retention_days" {
+  type        = number
+  description = "how much is the soft delete retention days for the key vault"
+  default     = 7
+}
+variable "purge_protection_enabled" {
+  type        = bool
+  description = "whether purge_protection is enabledfor the key vault"
+  default     = true
+}
+variable "sku_name" {
+  type        = string
+  description = "sku for the key vault"
+}
+variable "public_network_access_enabled" {
+  type        = string
+  description = "monitor_name for the key vault"
+}
+# variable "storage_name" {
+#   type        = string
+#   description = "storage_name for the key vault"
+# }
